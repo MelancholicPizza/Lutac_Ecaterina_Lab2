58fdd18354578449216040c148f511ab6eba5a4f
Lutac Ecaterina <ecaterinalutac99@gmail.com>
2025-12-14 17:29:56 +0200

    Rezolvare cerinte de baza

--- Migrations/20251214105528_Borrowings.Designer.cs
+++ Migrations/20251214105528_Borrowings.Designer.cs
@@ -4,6 +4,7 @@ using Lutac_Ecaterina_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lutac_Ecaterina_Lab2.Migrations
 {
     [DbContext(typeof(Lutac_Ecaterina_Lab2Context))]
-    partial class Lutac_Ecaterina_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20251214105528_Borrowings")]
+    partial class Borrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Pages/Members/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lutac_Ecaterina_Lab2.Data;
+using Lutac_Ecaterina_Lab2.Models;
+
+namespace Lutac_Ecaterina_Lab2.Pages.Members
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Lutac_Ecaterina_Lab2.Data.Lutac_Ecaterina_Lab2Context _context;
+
+        public IndexModel(Lutac_Ecaterina_Lab2.Data.Lutac_Ecaterina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Member> Member { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Member = await _context.Member.ToListAsync();
+        }
+    }
+}
